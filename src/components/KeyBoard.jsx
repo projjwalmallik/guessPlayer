8acbc41c4f03224d545a3317727c470cca391a26
jaymehta002 <jaymehta002@gmail.com>
2024-02-27 22:42:41 +0530

    new

--- src/components/KeyBoard.jsx
+++ src/components/KeyBoard.jsx
@@ -1,22 +1,25 @@
+import PropTypes from 'prop-types';
 
-const KeyBoard = ({onKeyPress}) => {
+const KeyBoard = ({ onKeyPress }) => {
     const keyboardLayout = [
         ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',],
         ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L',],
         ['Z', 'X', 'C', 'V', 'B', 'N', 'M', 'Del',],
         ['Hint', 'Space', 'Guess']
-      ];
-  return (
-    <>
-      <div className="keyboard ">
+    ];
+    return (
+        <div className="keyboard">
             {keyboardLayout.map((row, rowIndex) => (
                 <div key={rowIndex} className="keyboard-row">
                     {row.map((key, keyIndex) => (
                         <button
                             key={keyIndex}
                             onClick={() => onKeyPress(key)}
-                            className={key === "Space" ? "space-key" : ""}
-                            // disabled={checkDisableButton(key)}
+                            className={key === "Space" ? "space-key" :
+                            key === "Guess" ? "guess-key" :
+                            key === "Del" ? "del-key" :
+                            key === "Hint" ? "hint-key" :
+                            ""}
                         >
                             {key}
                         </button>
@@ -24,8 +27,11 @@ const KeyBoard = ({onKeyPress}) => {
                 </div>
             ))}
         </div>
-    </>
-  )
-}
+    );
+};
 
-export default KeyBoard
+KeyBoard.propTypes = {
+    onKeyPress: PropTypes.func.isRequired
+};
+
+export default KeyBoard;
